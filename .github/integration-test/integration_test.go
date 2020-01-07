844589dc659b64ccb2ea396345f46c509d8732b7
Atsushi Watanabe <atsushi.w@ieee.org>
2020-01-07 14:18:26 +0900

    Run integration test on GitHub Actions

---
+++ .github/integration-test/integration_test.go
@@ -0,0 +1,90 @@
+// +build integration
+
+package pulse
+
+import (
+	"testing"
+	"time"
+
+	"github.com/jfreymuth/pulse"
+)
+
+func TestIntegration(t *testing.T) {
+	var buf []int16
+
+	// Dummy loopback doesn't start immediately sometimes.
+	// Retry a few times.
+	for retry := 0; retry < 3; retry++ {
+		c, err := pulse.NewClient()
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer c.Close()
+
+		record, err := c.NewRecord(
+			func(in []int16) {
+				buf = append(buf, in...)
+			},
+			pulse.RecordAdjustLatency(true),
+			pulse.RecordBufferFragmentSize(256),
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var cnt uint32
+		playback, err := c.NewPlayback(
+			func(out []int16) {
+				// Play rectangular wave
+				for i, _ := range out {
+					if cnt%16 < 8 {
+						out[i] = 1000
+					} else {
+						out[i] = -1000
+					}
+					cnt++
+				}
+			},
+			pulse.PlaybackBufferSize(256),
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		record.Start()
+		playback.Start()
+		time.Sleep(time.Second)
+
+		record.Stop()
+		playback.Stop()
+		time.Sleep(100 * time.Millisecond)
+
+		if len(buf) > 256 {
+			break
+		}
+	}
+
+	if len(buf) < 256 {
+		t.Fatalf("Could not record enough number of the samples. (%d)", len(buf))
+	}
+
+	// Check recorded signal
+	var histogram [3]int
+	for _, v := range buf {
+		switch {
+		case v == -1000:
+			histogram[0]++
+		case v == 1000:
+			histogram[2]++
+		default:
+			histogram[1]++
+		}
+	}
+	if histogram[1] > len(buf)/100 {
+		t.Errorf("Recorded signal has values not in played signal.")
+	}
+	dutyRatio := float32(histogram[2]) / float32(histogram[0]+histogram[2])
+	if dutyRatio < 0.49 || 0.51 < dutyRatio {
+		t.Errorf("Duty ratio of the recorded signal is not matched to the played signal. (played: 0.5, recorded: %f)", dutyRatio)
+	}
+}
